1afa3c10067a7b9e76d6eb346da74e2e757c3c27
Daniel-Maslyanikov <mddm0113@gmail.com>
2023-02-03 20:36:08 +0300

    added pizza render

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 
+import pizzas from './assets/pizzas.json';
+
 function App() {
   return (
     <div className="wrapper">
@@ -17,13 +19,9 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-						<PizzaBlock title="Мексиканская" price={500}/>
-						<PizzaBlock />
-						<PizzaBlock />
-						<PizzaBlock />
-						<PizzaBlock />
-						<PizzaBlock />
-						<PizzaBlock />
+            {pizzas.map((obj) => (
+              <PizzaBlock {...obj}/>
+            ))}
           </div>
         </div>
       </div>
