0b5c4939e6c597adb0a9f1c77acdf681104093ae
Daniel-Maslyanikov <mddm0113@gmail.com>
2023-02-06 17:34:51 +0300

    added fetch method

--- src/App.js
+++ src/App.js
@@ -3,11 +3,24 @@ import './scss/app.scss';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
+import PizzaBlock from './components/PizzaBlock/PizzaBlock';
 
 import pizzas from './assets/pizzas.json';
+import React from 'react';
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://63e1011159bb472a742e4482.mockapi.io/items')
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
@@ -20,7 +33,7 @@ function App() {
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
             {pizzas.map((obj) => (
-              <PizzaBlock key={obj.id} {...obj}/>
+              <PizzaBlock key={obj.id} {...obj} />
             ))}
           </div>
         </div>
